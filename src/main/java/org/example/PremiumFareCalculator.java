990abb3917bfff5f2e3ad91ab943107982e7dc7f
jerome.yang <jerome.yang@gcash.com>
2025-07-04 13:38:13 +0800

    Revert "fix: changed distance and time multiplier for premium"
    This reverts commit 434b835fbb41a01e4fcac6a801c1e0ea65248beb.

--- src/main/java/org/example/PremiumFareCalculator.java
+++ src/main/java/org/example/PremiumFareCalculator.java
@@ -4,7 +4,7 @@ public class PremiumFareCalculator implements FareCalculator {
     @Override
     public double calculateFare(double distance, double time, boolean hasSurcharge){
         double baseFare = 60;
-        double fare = baseFare + distance * 12.5 + time * 3;
+        double fare = baseFare + distance * 10 + time * 2;
         if (hasSurcharge){
             fare += (fare * 1.35);
         }
